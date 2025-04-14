775576e0f6620ce712266891a82195278046b299
Yogesh <eyogesh>
2025-04-14 18:38:04 +0530

    deleted products done

--- Scripts/check.js
+++ Scripts/check.js
@@ -11,7 +11,8 @@ cart.forEach((cartItem)=>{
     }
   });
   cartSummaryHTML+=`
-    <div class="cart-item-container">
+    <div class="cart-item-container
+    js-cart-item-container-${matchProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -94,7 +95,8 @@ document.querySelectorAll('.js-delete-link')
   link.addEventListener('click',()=>{
     const productId=link.dataset.productId;
     removeCart(productId);
-
+    const container = document.querySelector(`.js-cart-item-container-${productId}`)
+    container.remove();
   });
   
 });
