3f2e6b8b88faac5fb197894043a70366f88370b5
Yogesh <eyogesh>
2025-04-14 16:48:50 +0530

    tillnow

---
+++ Scripts/amazon.js
@@ -0,0 +1,85 @@
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
+ 
+let productHTMl='';
+products.forEach((product) => {
+  productHTMl+=`
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars *10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents/100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-cart-button" 
+          data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>
+  `
+});
+document.querySelector('.products-grid').innerHTML = productHTMl;
+document.querySelectorAll('.js-cart-button').forEach(button => {
+  button.addEventListener('click', () => {
+    //const productsdsaa = button.dataset.productId;
+    const productId = button.dataset.productId;
+
+    let matchItem;
+    cart.forEach((item) =>{
+      if(productId === item.productId){
+        matchItem=item;
+      }
+    });
+    if(matchItem){
+      matchItem.quantity += 1;
+    }else{
+      cart.push({
+      productId : productId,
+      quantity : 1
+    });
+    } 
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity+=item.quantity;
+      document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+    })
+  });
+});
+
