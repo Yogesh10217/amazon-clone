ab5efe4af780c9104163273ccdfff0f6e3719049
Yogesh <eyogesh>
2025-04-14 18:29:02 +0530

    cart deleting survey

--- Scripts/check.js
+++ Scripts/check.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,removeCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 let cartSummaryHTML='';
 cart.forEach((cartItem)=>{
@@ -34,7 +34,7 @@ cart.forEach((cartItem)=>{
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link"data-product-id = "${matchProduct.id}">
               Delete
             </span>
           </div>
@@ -47,7 +47,7 @@ cart.forEach((cartItem)=>{
           <div class="delivery-option">
             <input type="radio" checked
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Tuesday, June 21
@@ -60,7 +60,7 @@ cart.forEach((cartItem)=>{
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Wednesday, June 15
@@ -73,7 +73,7 @@ cart.forEach((cartItem)=>{
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Monday, June 13
@@ -89,6 +89,15 @@ cart.forEach((cartItem)=>{
   `;  
 });
 document.querySelector('.order-summary').innerHTML=cartSummaryHTML;
+document.querySelectorAll('.js-delete-link')
+.forEach((link)=>{
+  link.addEventListener('click',()=>{
+    const productId=link.dataset.productId;
+    removeCart(productId);
+
+  });
+  
+});
 
 
 
