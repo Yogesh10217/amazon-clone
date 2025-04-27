7eeaeab47c844a5dc9b61486be26793feec50bdd
Yogesh <eyogesh>
2025-04-27 19:21:09 +0530

    addind framework jasmine

---
+++ test/moneyTest.js
@@ -0,0 +1,23 @@
+import {formatCurrency} from "../Scripts/utility/money.js";
+
+console.log("test suit: formatCurrency")
+console.log("convert cents into dollars")
+if(formatCurrency(2095)==='20.95'){
+  console.log('passed');
+}else{
+  console.log('failled');
+}
+
+console.log("works with zero")
+if(formatCurrency(0)==='0.00'){
+  console.log('passed');
+}else{
+  console.log('failled');
+}
+
+console.log("rounds up to the nearest cents")
+if(formatCurrency(2000.5) === '20.01'){
+  console.log('passed');
+}else{
+  console.log('failled');
+}
